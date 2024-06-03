64c929dc625c34ebb230bd583e76fd9b4036dff2
Max van Eikeren <maxvaneikeren@gmail.com>
2024-06-03 16:07:02 +0200

    Added Hotwheels Launch

---
+++ Hotwheels.py
@@ -0,0 +1,66 @@
+import RPi.GPIO as GPIO  # type: ignore
+import time
+
+
+# Set up GPIO pin
+
+
+# Function to measure distance
+def distance(GPIO_ECHO, GPIO_TRIGGER):
+    # Set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # Set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    start_time = time.time()
+    stop_time = time.time()
+
+    # Save start time
+    while GPIO.input(GPIO_ECHO) == 0:
+        start_time = time.time()
+
+    # Save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        stop_time = time.time()
+
+    # Time difference   start and arrival
+    time_elapsed = stop_time - start_time
+
+    # Multiply with the speed of sound (34300 cm/s) and divide by 2
+    distance = (time_elapsed * 34300) / 2
+
+    return distance
+
+
+# Function to control the solenoid
+def loop(solenoid_pin, GPIO_ECHO, GPIO_TRIGGER):
+    try:
+        while True:
+            dist = distance()
+            if (
+                dist < 10
+            ):  # Adjust this value to trigger the solenoid at a certain distance
+                GPIO.output(solenoid_pin, GPIO.HIGH)  # Turn on the solenoid
+                print("Solenoid turned on")
+            else:
+                GPIO.output(solenoid_pin, GPIO.LOW)  # Turn off the solenoid
+                print("Solenoid turned off")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
+
+
+def main(GPIO_ECHO, GPIO_TRIGGER, solenoid_pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+    GPIO.setup(GPIO_ECHO, GPIO.IN)
+    GPIO.setup(solenoid_pin, GPIO.OUT)
+    loop(solenoid_pin, GPIO_ECHO, GPIO_TRIGGER)
+
+
+# Run the loop function
+if __name__ == "__main__":
+    loop()
--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import time
 import Elevator
 import ServoRazorMotor
 import Dominoes
+import Hotwheels
 
 HammerServoPin = #any #type: ignore
 HammerButtonPin = #any #type: ignore
@@ -9,6 +10,10 @@ HammerButtonPin = #any #type: ignore
 ElevatorControlPins = #any #type: ignore
 ElevatorEndstopPin = #any #type: ignore
 ElevatorShockSensorPin = #any #type: ignore
+
+DistanceEcho = #any #type: ignore
+DistanceTrig = #any #type: ignore
+CarSolenoid = #any #type: ignore
 
 JoyInPin = #ADCChannel #type: ignore
 
@@ -25,9 +30,20 @@ if input("start program? y/n\n") == "y":
     Elevator.main(ElevatorControlPins,ElevatorEndstopPin, ElevatorShockSensorPin)
     print("Elevator Finished")
     time.sleep(0.5)
+    print("Hotwheels Released")
+    Hotwheels.main(DistanceEcho,DistanceTrig,CarSolenoid)
+    print("Hotwheels Finished")
+    time.sleep(0.5)
+    #print("Launching Ball")
+    #Launch.main(pins)
+    #print("Ball landed")
+    time.sleep(0.5)
     print("Dominoes Starting")
     Dominoes.main(DcMotorInA,DcMotorInB)
     print("Dominoes Finished")
     time.sleep(0.5)
+    #print("Launching Plane")
+    #Plane.main(pins)
+    #print("Program Finished")
 else:
     exit(1)
